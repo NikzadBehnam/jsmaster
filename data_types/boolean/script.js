88918a6572a5b4384dfb82d6e449e786f9fdf5f1
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-08-27 12:37:18 +0200

    Update: Example on booleans DataTypes

---
+++ data_types/boolean/script.js
@@ -0,0 +1,21 @@
+let isLoggedIn = true;
+let hasPermission = true;
+
+
+
+/* 
+
+قبل از این که به دستورات شرطی برسیم، برای این که مفهوم درست بولین را درک کنیم یک مثال بیسار ابتدایی از دستور اف  را میبینیم 
+
+*/
+
+// if (hasPermission) {
+//   console.log("you are eligible !");
+// }
+// console.log("out of IF");
+
+
+
+// const temperatureDiv = document.querySelector("#temp");
+// temperatureDiv.textContent = temperature;
+// console.log(tempDiv);
