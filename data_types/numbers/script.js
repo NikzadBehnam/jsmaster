21af1dfb56ffa5cc7b955000166c29aeee39c2ab
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-08-27 12:12:26 +0200

    Update: Example on number types

---
+++ data_types/numbers/script.js
@@ -0,0 +1,13 @@
+const age = 25;
+const price = 19.99;
+const temperature = -4;
+
+console.log("age: ", age);
+console.log(`price : ${price}`);
+
+
+
+// if you bored of printing on console. let me show you a small DOM manipulation before we reach to DOM section
+// const temperatureDiv = document.querySelector("#temp");
+// temperatureDiv.textContent = temperature;
+// console.log(tempDiv);
