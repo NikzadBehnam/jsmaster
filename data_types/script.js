5afa475a3a6642daeae0a8105557166e5d517b13
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-08-27 11:34:53 +0200

    Update: example on basic strings

---
+++ data_types/script.js
@@ -0,0 +1,10 @@
+// Single quotes
+const firstName = "ALice";
+
+// double quotes
+const greetinge = "Hello World !";
+
+// string concatenation with template litterals
+const message = `Welcome, ${firstName}`;
+
+console.log(message);
