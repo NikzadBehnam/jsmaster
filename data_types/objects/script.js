51b67249ab27be32601c8ea1232a6d14c72d151e
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-08-29 22:35:22 +0200

    Update: Example on Object  DataTypes

---
+++ data_types/objects/script.js
@@ -0,0 +1,50 @@
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
+// console.log("His Full name: ", person.firstName + " " + person.lastName);
+console.log(`His full name : ${person.firstName} ${person.lastName}`);
+
+console.log("His age: ", person.age);
+/* 
+
+آبجکت هانند هر وریابل دیگر توسط لت، ور، یا کانست تعریف میشود
+معلومات یا دیتا مربوط به آبجک در داخل کرلی بریسس یا قوسط بزرگ نوشته نوشته میشود
+معلومات ذخیره شده در داخل آبجکت  به کی و والیو پیرس  نمایش داده مشود
+معموله آبجک وقتی استفاده میشود که وریابل مورد نظر ما بیش از یک مشخصه دارد برای استفاده 
+
+
+به طور مثال ما اگر یک وریابل داشته باشیم به نام کار یا موتر، این اسم یک اسم کلی میباشد و به هر نوع موتر دلالت میکند
+ولی با استفاده از آبجک میتوانیم در زنگ و مودل و کامپنی تولیدی آن تفکیک کنیم به طور مثال
+
+let car = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2022,
+  color: "Blue",
+  mileage: 15000
+};
+
+let laptop = {
+  brand: "Apple",
+  model: "MacBook Pro",
+  year: 2023,
+  processor: "M2",
+  ram: "16GB",
+  storage: "512GB SSD",
+  screenSize: "14-inch"
+};
+
+
+
+*/
+
+// if (hasPermission) {
+//   console.log("you are eligible !");
+// }
+// console.log("out of IF");
+
+// const temperatureDiv = document.querySelector("#temp");
+// temperatureDiv.textContent = temperature;
+// console.log(tempDiv);
