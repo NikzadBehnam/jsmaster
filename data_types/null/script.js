47d5a9b186458ae10133b9536164bf6e63defc55
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-08-29 21:44:49 +0200

    Update: Example on null DataTypes

---
+++ data_types/null/script.js
@@ -0,0 +1,15 @@
+let emptyValue = null;
+console.log(emptyValue);
+
+/* 
+
+استفاده این نوع دیتا، هنگامی اتفاق میفتد که ما ضرورت دارشته باشیم یک وریابل را بعد از اجرای یک سری از اجرات خالی نماییم و برای ایجرای بعدی آماده سازیم
+که در جریان پروژه استفاده آن را بهتر متوجه میشوید 
+
+فعلا همین اندازه بدانید که ما توسط این نوع قیمت گذاری میتوانیم قیمت وریابل را خالی تعیین کنیم
+
+*/
+
+// const temperatureDiv = document.querySelector("#temp");
+// temperatureDiv.textContent = temperature;
+// console.log(tempDiv);
