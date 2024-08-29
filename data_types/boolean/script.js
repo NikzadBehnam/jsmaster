423ea6d02553fbbae752de00c3363ac7241bc7bb
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-08-29 16:51:20 +0200

    Update: Example on undefined DataTypes

--- data_types/boolean/script.js
+++ data_types/boolean/script.js
@@ -5,7 +5,7 @@ let hasPermission = true;
 
 /* 
 
-قبل از این که به دستورات شرطی برسیم، برای این که مفهوم درست بولین را درک کنیم یک مثال بیسار ابتدایی از دستور اف  را میبینیم 
+قبل از این که به دستورات شرطی برسیم، برای این که مفهوم درست بولین را درک کنیم یک مثال بسیار ابتدایی از دستور اف  را میبینیم 
 
 */
 
@@ -13,7 +13,6 @@ let hasPermission = true;
 //   console.log("you are eligible !");
 // }
 // console.log("out of IF");
-
 
 
 // const temperatureDiv = document.querySelector("#temp");
---
+++ data_types/undefined/script.js
@@ -0,0 +1,14 @@
+let notAssigned;
+console.log(notAssigned); // undefined
+
+/* 
+
+ما مجهول و یا وریابل که قیمت نداشته باشد را نمیتوانیم با کانست تعریف کنیم
+همانطور که از اسم این دیتاتایپ میتوان تشخیص داد، این دیتا تایپ دلالت میکند به آن وریابل که قیمت ندارد
+
+*/
+
+
+// const temperatureDiv = document.querySelector("#temp");
+// temperatureDiv.textContent = temperature;
+// console.log(tempDiv);
